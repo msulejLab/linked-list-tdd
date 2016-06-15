38edf6f8faf236d402e094b0c98baa0a70a38158
Maciej Sulej <187809@edu.p.lodz.pl>
2016-06-15 21:51:18 +0200

    initial commit

---
+++ src/test/java/com/msulej/linkedlist/ListTest.java
@@ -0,0 +1,15 @@
+package com.msulej.linkedlist;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class ListTest {
+
+    @Test
+    public void itCompiles() {
+        assertThat(true, equalTo(true));
+    }
+}
